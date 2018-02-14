10021ee015df2b2c89746ab1848889b2d8824ef0
Mikhail Lysyansky <mikhail.lysyansky@jetruby.com>
2018-02-14 16:06:29 +0300

    Step 1 - Set Camera

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,25 +3,33 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class MyGdxGame extends ApplicationAdapter {
+    private static final float SCALE = 2.0f;
+    public static final float PIXEL_PER_METER = 32f;
 
-	
-	@Override
-	public void create () {
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    private OrthographicCamera orthographicCamera;
 
-	}
-	
-	@Override
-	public void dispose () {
+    @Override
+    public void create() {
+        orthographicCamera = new OrthographicCamera();
+        orthographicCamera.setToOrtho(false, Gdx.graphics.getWidth() / SCALE, Gdx.graphics.getHeight() / SCALE);
+    }
 
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        orthographicCamera.setToOrtho(false, width / SCALE, height / SCALE);
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
