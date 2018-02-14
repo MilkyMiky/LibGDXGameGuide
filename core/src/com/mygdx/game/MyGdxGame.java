7ce4dbff7f9d01b64473931722d221e55aed9d82
Mikhail Lysyansky <mikhail.lysyansky@jetruby.com>
2018-02-14 16:04:36 +0300

    Step 0 - Create project

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,27 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+
+	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
