4f6005e35b268b949b716fe4814e1cf33663a62b
Mikhail Lysyansky <mikhail.lysyansky@jetruby.com>
2018-02-14 16:12:08 +0300

    Step 2 - Create World and add Player.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,32 +4,58 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.entities.Player;
 
 public class MyGdxGame extends ApplicationAdapter {
     private static final float SCALE = 2.0f;
     public static final float PIXEL_PER_METER = 32f;
-
+    private static final float TIME_STEP = 1 / 60f;
+    private static final int VELOCITY_ITERATIONS = 6;
+    private static final int POSITION_ITERATIONS = 2;
 
     private OrthographicCamera orthographicCamera;
-
+    private Box2DDebugRenderer box2DDebugRenderer;
+    private World world;
+    private Player player;
+    private static final float VELOCITY_Y = -9.85f;
+    private static final float VELOCITY_X = 0f;
     @Override
-    public void create() {
+    public void create () {
         orthographicCamera = new OrthographicCamera();
         orthographicCamera.setToOrtho(false, Gdx.graphics.getWidth() / SCALE, Gdx.graphics.getHeight() / SCALE);
+        world = new World(new Vector2(VELOCITY_X, VELOCITY_Y), false);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+        player = new Player(world);
     }
-
     @Override
-    public void render() {
+    public void render () {
+        update();
         Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        box2DDebugRenderer.render(world, orthographicCamera.combined.scl(PIXEL_PER_METER));
     }
-
+    private void update() {
+        world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+        cameraUpdate();
+    }
+    private void cameraUpdate() {
+        Vector3 position = orthographicCamera.position;
+        position.x = player.getBody().getPosition().x * PIXEL_PER_METER;
+        position.y = player.getBody().getPosition().y * PIXEL_PER_METER;
+        orthographicCamera.position.set(position);
+        orthographicCamera.update();
+    }
     @Override
     public void resize(int width, int height) {
         orthographicCamera.setToOrtho(false, width / SCALE, height / SCALE);
     }
-
     @Override
     public void dispose() {
+        box2DDebugRenderer.dispose();
+        world.dispose();
     }
 }
---
+++ core/src/com/mygdx/game/entities/Player.java
@@ -0,0 +1,43 @@
+package com.mygdx.game.entities;
+
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.MyGdxGame;
+
+public class Player {
+    private static final int BOX_SIZE = 32;
+    private static final float PLAYER_DENSITY = 1.0f;
+    public static final float JUMP_FORCE = 250f;
+    public static final float RUN_FORCE = 5f;
+    public static final String PLAYER_IMG_PATH = "cat.png";
+    private static final float PLAYER_START_X = 8f;
+    private static final float PLAYER_START_Y = 18f;
+    private Body body;
+
+    public Player(World world) {
+        createBoxBody(world, PLAYER_START_X, PLAYER_START_Y);
+    }
+
+    private void createBoxBody(World world, float x, float y) {
+        BodyDef bdef = new BodyDef();
+        bdef.fixedRotation = true;
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        bdef.position.set(x, y);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(BOX_SIZE / MyGdxGame.PIXEL_PER_METER / 2, BOX_SIZE / MyGdxGame.PIXEL_PER_METER / 2);
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = PLAYER_DENSITY;
+        body = world.createBody(bdef);
+        body.createFixture(fixtureDef).setUserData(this);
+    }
+    public Body getBody() {
+        return body;
+    }
+
+
+}
